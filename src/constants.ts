888f15d10c0d3ae1ad0264dd3d368f53babf00b9
HynekS <hyneksvacha@seznam.cz>
2022-08-05 09:22:03 +0200

    Initial commit

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const DOMAIN_PATTERN = "*://*.csfd.cz/*";
+
+export const PREFIX = "__csfd-blocker";
---
+++ webpack.config.js
@@ -0,0 +1,85 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: {
+    popup: "./src/popup.tsx",
+    background: "./src/background.ts",
+    "idle-script": "./src/idle-script.tsx",
+    "start-script": "./src/start-script.ts",
+    "injected-styles": "./src/injected-styles.scss",
+  },
+  output: {
+    filename: "[name].js",
+    path: __dirname + "/dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(j|t)sx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/env",
+              ["@babel/preset-typescript", { jsxPragma: "h" }],
+            ],
+            plugins: [["@babel/transform-react-jsx", { pragma: "h" }]],
+          },
+        },
+      },
+      {
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: "javascript/auto",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.scss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          //"style-loader",
+          "css-loader",
+          "resolve-url-loader",
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx", ".mjs"],
+
+    alias: {
+      react: "preact/compat",
+      "react-dom/test-utils": "preact/test-utils",
+      "react-dom": "preact/compat", // Must be below test-utils
+      "react/jsx-runtime": "preact/jsx-runtime",
+    },
+  },
+  plugins: [
+    new FixStyleOnlyEntriesPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: __dirname + "/manifest.json",
+          to: __dirname + "/dist/manifest.json",
+        },
+        {
+          from: __dirname + "/src/popup.html",
+          to: __dirname + "/dist/popup.html",
+        },
+      ],
+    }),
+  ],
+  devtool: "source-map",
+  watch: true,
+};
