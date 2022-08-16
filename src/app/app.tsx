47413a55a623b0460c7e077d6cf689affefbb381
HynekS <hyneksvacha@seznam.cz>
2022-08-16 14:16:28 +0200

    Add relative date feature

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -15,6 +15,40 @@ function ObjectEntries<T extends object>(t: T): Entries<T>[] {
   return Object.entries(t) as any;
 }
 
+type Unit = keyof typeof units;
+
+const LOCALE = "en-GB";
+
+const units = {
+  year: 24 * 60 * 60 * 1000 * 365,
+  month: (24 * 60 * 60 * 1000 * 365) / 12,
+  week: 24 * 60 * 60 * 1000 * 7,
+  day: 24 * 60 * 60 * 1000,
+  hour: 60 * 60 * 1000,
+  minute: 60 * 1000,
+  second: 1000,
+};
+
+const getRelativeTime = (
+  timestamp: number,
+  limit = units.week,
+  locale = LOCALE
+): string | null => {
+  const rtf: Intl.RelativeTimeFormat = new Intl.RelativeTimeFormat(locale, {
+    numeric: "auto",
+  });
+
+  const elapsed = timestamp - new Date().getTime();
+
+  if (Math.abs(elapsed) > limit) return null;
+
+  for (const u of Object.keys(units)) {
+    if (Math.abs(elapsed) > units[u as Unit] || u == "second")
+      return rtf.format(Math.round(elapsed / units[u as Unit]), u as Unit);
+  }
+  return null;
+};
+
 const ListItem = ({
   item,
 }: {
@@ -31,10 +65,18 @@ const ListItem = ({
     );
   };
 
+  const timestamp = new Date(item.dateAdded).getTime();
+  const relativeDate = getRelativeTime(timestamp);
+
   return (
     <li class="border rounded py-1 px-2 mb-1">
-      <strong>{item.userNamePreservedCase ?? item.userNameLowerCase}</strong>
-      <small>added {new Date(item.dateAdded).toLocaleTimeString()}</small>
+      <div>
+        <strong>{item.userNamePreservedCase ?? item.userNameLowerCase}</strong>
+      </div>
+      <small>
+        added{" "}
+        {relativeDate ?? new Date(item.dateAdded).toLocaleDateString(LOCALE)}
+      </small>
       <button onClick={handleClick}>remove</button>
     </li>
   );
