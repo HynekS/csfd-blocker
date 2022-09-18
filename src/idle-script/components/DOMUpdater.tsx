cf609c66221c64c35eafd5d0f0dbc8044ca98a58
HynekS <hyneksvacha@seznam.cz>
2022-09-19 00:43:29 +0200

    Make DOM updates in useEffect

--- src/idle-script/components/DOMUpdater.tsx
+++ src/idle-script/components/DOMUpdater.tsx
@@ -3,10 +3,22 @@ import { useGlobalState } from "../../state";
 
 export default function Updater() {
   const [blocklist, _] = useGlobalState("blocklist");
+
+  const ratings = document.querySelectorAll(".box-rating ul li a");
+
   useEffect(() => {
-    // update DOM
+    ratings.forEach((node) => {
+      if (
+        blocklist.hasOwnProperty(node?.textContent?.trim().toLowerCase() ?? "")
+      ) {
+        (node as HTMLElement).style.display = "none";
+      } else {
+        (node as HTMLElement).style.removeProperty("display");
+      }
+    });
 
     console.log("blocklist has changed!");
+
     return () => {};
   }, [JSON.stringify(blocklist)]);
   return null;
